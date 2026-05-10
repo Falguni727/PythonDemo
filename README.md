6bbbeb6b355949f25f2f26414f2b1a32bf03bf60
Kushal Harsora <kushalharsora13@gmail.com>
2026-05-10 13:41:13 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Python Demo Assignment
+
+Python assignment where you are to change the datatype of user-id from integer to string
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/KushalHarsora/PythonDemo.git
+```
+
+Go to the project directory
+
+```bash
+  cd PythonDemo
+```
+
+Install dependencies
+
+```bash
+  pip install -r requirements.txt
+```
+
+Start the server
+
+```bash
+  python app.py
+```
+
+## Create the new get request
+
+create a new patch request with the path /patch with the requirements mentioned in the code file. Once done submit the code by creating a fork of repo and creating a new pull request.
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Flask Demo App</h1>
+    <p>Navigate to the <strong>test</strong> route and begin the assignment</p>
+    """
+
+@app.route("/test")
+def test():
+    data = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    return data.json()
+
+
+@app.route("/patch")
+def patch():
+
+    """
+    BEGIN THE PATCHES IN THIS FUNCTION
+    PRINT THE DATA TYPES OF ALL THE ATTRIBUTES
+    CHANGE THE TYPE OF USER-ID TO STRING FROM INTEGER
+    AND RETURN THE NEW JSON AS RESPONSE
+    :return:
+    """
+    data = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    # BEGIN THE ASSIGNMENT FROM HERE
+
+
+
+    return data.json()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
